47a78e07cc3cb4dad126fa6204e35b1178609f29
Tychodiep <145542519+Tychodiep@users.noreply.github.com>
2024-12-11 15:53:05 +0100

    final

--- scripts/script.js
+++ scripts/script.js
@@ -18,3 +18,29 @@ zoekButton.addEventListener("click", toggleZoeken);
 function toggleZoeken() {
     deZoek.classList.toggle("toonZoek");
 }
+
+
+
+const headerShareButton = document.getElementById("header-share-button");
+const articleShareButtons = document.querySelectorAll("article li button");
+
+articleShareButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      
+        const Active = button.classList.contains("active");
+
+      
+        let currentCount = parseInt(headerShareButton.getAttribute("data-share-count"), 10); /*chatgbt*/
+
+        if (Active) {
+            button.classList.remove("active");
+            currentCount--;} 
+            else {
+            button.classList.add("active");
+            currentCount++;
+        }
+
+        headerShareButton.setAttribute("data-share-count", currentCount); /*chatgbt*/
+        headerShareButton.innerHTML = `<img src="images/deel-button-ajax.svg" alt="deel icoontje">(${currentCount})`; /*chatgbt*/
+    });
+});
